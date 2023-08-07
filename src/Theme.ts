a1d2fe0733be5be5292305185225eac7924e9987
Nicolás Antonelli <niconelli2@gmail.com>
2023-08-07 04:12:48 -0300

    Add Theme

---
+++ src/Theme.ts
@@ -0,0 +1,25 @@
+const Theme = {
+    colors: {
+        textPrimary: '#24292e',
+        textSecondary: '#586069',
+        primary: '#0366d6',
+        secondary: '#dddddd'
+    },
+    fontSizes: {
+        title: 20,
+        subHeader: 16,
+        body: 14,
+        small: 12
+    },
+    fonts: {
+        main: 'System'
+    },
+    fontWeights: {
+        normal: '400' as Weight,
+        bold: '700' as Weight
+    }
+}
+
+type Weight = "400" | "700" | "normal" | "bold" | "100" | "200" | "300" | "500" | "600" | "800" | "900" | undefined
+
+export default Theme
